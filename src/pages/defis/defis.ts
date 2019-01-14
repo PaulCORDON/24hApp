6afe629b31f0d285c8d4abd99acfd29e9dfc3800
Unknown <matthieurode@yahoo.fr>
2019-01-14 10:19:49 +0100

    Revert "retirer insulte"
    This reverts commit d38beaff4923248d3c45be3d0cef390b93d38468.

--- src/pages/defis/defis.ts
+++ src/pages/defis/defis.ts
@@ -17,10 +17,9 @@ import { SQLiteService } from '../../SQLite/SQLiteService';
 export class DefisPage {
 
   listeDefis: any =
-    [{ "id":"101","titre": "Test 1", "etat": "debloque","theme":"100"},{"id":"102","titre":"Wesh lol","etat":"bloque","theme":"200"}];
+    [{ "id":"101","titre": "Niquez des mères", "etat": "debloque","theme":"100"},{"id":"102","titre":"Wesh lol","etat":"bloque","theme":"200"}];
   listeThemes: any=
     [{ "id":"100","titre": "Le code / La programmation"},{"id":"200","titre":"Les femmes dans l'informatique"}];
-  nom:any;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public data : SQLiteService) {
     data.selectData(200, "theme", "*").then((data)=>
