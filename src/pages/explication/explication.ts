548c154c7935e592690fc33ee5bc12aba3053230
Paul <paul.cordon@laposte.net>
2019-01-17 19:25:15 +0100

    le ticket s'affiche bien que quand il a le droit à la fin de la dernière explication il reste à l'animer

--- src/pages/explication/explication.ts
+++ src/pages/explication/explication.ts
@@ -28,6 +28,7 @@ export class ExplicationPage {
   toast: boolean;
   explication: any;
   isNextQuestion: boolean;
+  newTicket : boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public sqlLite: SQLiteService) {
     this.idDefi = navParams.get("idDefi");
@@ -46,33 +47,33 @@ export class ExplicationPage {
   ionViewDidLoad() {
     console.log('----------------------- ExplicationPage');
     this.isNextQuestion = (this.nbQuestion > this.numQuestion);
+    
+
+    if(!this.isNextQuestion){
+      this.sqlLite.selectData("where `id` = " + this.idDefi, "defi", "etat").then((defiListe) => {
+        if (defiListe[0].etat != 2) {
+          this.newTicket=true;
+          console.log("QUESTION --- Etat du défi : " + JSON.stringify(defiListe[0]));
+          GlobalVarsProvider.instance.updateNombreTicket(1);  
+          this.sqlLite.setData("defi", "etat", 2, "where `id` = " + this.idDefi);
+          this.sqlLite.setData("theme", "nbTicketActuel", 1, "where `id` = " + (this.listDefi[this.idDefi - 1].idTheme));  
+          this.sqlLite.setData("defi", "etat", 1, "where `id` = " + (++this.idDefi));
+        }
+  
+        console.log("QUESTION --- Listes des réponses : ", this.listReponses)
+        console.log("QUESTION --- Numéro de la question (affichage) : ", this.numQuestion + "/" + this.nbQuestion);
+      });
+    }
   }
 
   onClickTerminer() {
     console.log("QUESTION --- C'était la dernière question");
-        
-
-        this.sqlLite.selectData("where `id` = " + this.idDefi, "defi", "etat").then((defiListe) => {
-          if (defiListe[0].etat != 2) {
-            console.log("QUESTION --- Etat du défi : " + JSON.stringify(defiListe[0]));
-            GlobalVarsProvider.instance.updateNombreTicket(1);
-
-            this.sqlLite.setData("defi", "etat", 2, "where `id` = " + this.idDefi);
-            this.sqlLite.setData("theme", "nbTicketActuel", 1, "where `id` = " + (this.listDefi[this.idDefi - 1].idTheme));
-            
-            this.sqlLite.setData("defi", "etat", 1, "where `id` = " + (++this.idDefi));
-          }
-          
-          console.log("QUESTION --- Listes des réponses : ", this.listReponses)
-          console.log("QUESTION --- Numéro de la question (affichage) : ", this.numQuestion + "/" + this.nbQuestion);
-        });
-      this.navCtrl.push(DefisPage);
+    this.navCtrl.push(DefisPage);
   }
 
   onClickNextQuestion() {
     console.log("EXPLICATION --- Click");
     console.log(this.nbQuestion + " > " + this.numQuestion)
-
     this.navCtrl.push('QuestionPage', { idDefi: this.idDefi, question: this.question, numQuestion: this.numQuestion, nbQuestion: this.nbQuestion, listDefi: this.listDefi });
   }
 
